9124f7dbf305eeaf2cb1814ec684ea732329e2a9
vetal4x <vetal4x@gmail.com>
2025-07-18 02:02:51 +0200

    Mobile version started. Added header, humburger, hero section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Hamburger
+
+const header = document.querySelector('.header');
+const hamburger = document.querySelector('.header__hamburger');
+
+hamburger.addEventListener('click', () => {
+  header.classList.toggle('open');
+});
