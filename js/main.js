b2b4a635280a21ce5a7e4c0f295779206eaa1f14
vetal4x <vetal4x@gmail.com>
2025-07-19 01:26:47 +0200

    Added Our Rooms Section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,17 @@ const hamburger = document.querySelector('.header__hamburger');
 hamburger.addEventListener('click', () => {
   header.classList.toggle('open');
 });
+
+const wrappers = document.querySelectorAll('.accordion__wrapper');
+
+wrappers.forEach(wrapper => {
+  const label = wrapper.querySelector('.our-rooms__accordion-label');
+
+  label.addEventListener('click', () => {
+    wrappers.forEach(w => {
+      if (w !== wrapper) w.classList.remove('active');
+    });
+    wrapper.classList.toggle('active');
+  });
+});
+
