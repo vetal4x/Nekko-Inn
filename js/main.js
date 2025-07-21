da0cc585fd6fe8b83d8ed860ea8719d415b2a091
vetal4x <vetal4x@gmail.com>
2025-07-21 22:41:57 +0200

    Added Slider

--- js/main.js
+++ js/main.js
@@ -7,16 +7,48 @@ hamburger.addEventListener('click', () => {
   header.classList.toggle('open');
 });
 
+// Accordion
+
 const wrappers = document.querySelectorAll('.accordion__wrapper');
 
-wrappers.forEach(wrapper => {
+wrappers.forEach((wrapper) => {
   const label = wrapper.querySelector('.our-rooms__accordion-label');
 
   label.addEventListener('click', () => {
-    wrappers.forEach(w => {
+    wrappers.forEach((w) => {
       if (w !== wrapper) w.classList.remove('active');
     });
     wrapper.classList.toggle('active');
   });
 });
 
+// Slider
+
+const swiper = new Swiper('.swiper', {
+  grabCursor: true,
+  effect: 'creative',
+  speed: 700,
+  loop: true,
+  navigation: {
+    nextEl: '.button-next',
+    prevEl: '.button-prev',
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  creativeEffect: {
+    prev: {
+      translate: [0, 0, -400],
+      scale: 0.9,
+      rotate: [0, 0, -8],
+      opacity: 0.8,
+    },
+    next: {
+      translate: ['100%', 0, 0],
+      scale: 0.9,
+      rotate: [0, 0, 8],
+      opacity: 0.8,
+    },
+  },
+});
